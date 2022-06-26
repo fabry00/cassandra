69a151aa9b20bd76f21a842b85e37e9a3e454532
fabry00 <fabry_00@hotmail.com>
2022-06-26 09:45:42 +0200

    aa

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker-compose down
+docker-compose  -f docker-compose_win.yml down || true
+docker-compose  -f docker-compose_os.yml down || true
 
 read -p "Delete the volume? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # do dangerous stuff
-    docker volume rm cassandra_Cassandra_data
+    docker volume rm cassandra_cassandra_data
 fi
